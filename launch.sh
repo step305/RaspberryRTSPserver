971ce8d1f9f7a95d6a240eaa0cbee5c6810c06b8
step305 <31113900+step305@users.noreply.github.com>
2020-06-11 06:15:18 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./RTSPserver &
-raspivid -t 0 -h 720 -w 1280 -fps 25 -b 8000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=127.0.0.1 port=5000 &
+raspivid -t 0 -h 540 -w 960 -fps 25 -b 4000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=127.0.0.1 port=5000 &
